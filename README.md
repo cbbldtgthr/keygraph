4d34488e218efdbcca97074c7adf6a69dcf50a85
Andrew Cloete <andrew.cloete@spatialedge.co.za>
2024-10-19 06:05:54 +0200

    Completes most of the mappings

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ cargo run --bin keyserver
 npx serve
 open -na "Brave Browser" --args  --incognito --app="http://localhost:3000/test.html"
 ```
+
+
--- app/script.js
+++ app/script.js
@@ -3,30 +3,27 @@ const eventsList = document.getElementById("events");
 
 ws.onmessage = (event) => {
   let msg = JSON.parse(event.data);
-  console.log(msg);
-  // Get the SVG element by ID
+  msg.key = msg.key.replace("(", "_").replace(")", "_");
+  console.log(msg, msg.name.charCodeAt(0));
   const svgDoc = document.getElementById("keyboard-svg").contentDocument;
-  let svgKey = svgDoc.getElementById(msg.key);
-  if (!svgKey) {
-    svgKey = svgDoc.getElementById(msg.name);
-    // console.log(msg.name);
-  }
+  let svgKey = svgDoc.getElementById(msg.name.charCodeAt(0));
   if (!svgKey) {
     svgKey = svgDoc.getElementById(msg.key + msg.name);
-    // console.log(msg.key + msg.name);
   }
   if (!svgKey) {
-    svgKey = svgDoc.getElementById(msg.name.charCodeAt(0));
+    svgKey = svgDoc.getElementById(msg.name);
+  }
+  if (!svgKey) {
+    svgKey = svgDoc.getElementById(msg.key);
   }
   if (!svgKey) {
     return;
   }
-  // Update the fill property
-  svgKey.style.fill = "green";
-
+  const DELAY_MS = 300;
+  svgKey.style.fill = "black";
   setTimeout(() => {
     svgKey.style.fill = "none";
-  }, 100);
+  }, DELAY_MS);
 };
 
 ws.onopen = () => {
